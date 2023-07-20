22454ef6da24d254ba1853be698354bdbad1b91c
Zgibbs58 <zacharywgibbs@gmail.com>
2023-07-19 21:44:53 -0500

    Set up the Associations models index.js to create relationships between product and category, then product and tag.

--- models/Product.js
+++ models/Product.js
@@ -30,7 +30,10 @@ Product.init(
     },
     category_id: {
       type: DataTypes.INTEGER,
-      allowNull: false,
+      references: {
+        model: "category",
+        key: "id",
+      },
     },
   },
   {
